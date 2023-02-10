86f6ab447097563d470937ae3c0ff98940b390e9
Alexandra Skripkina <aa.skripkina@gmail.com>
2023-02-10 20:33:52 +0200

    Initial commit

---
+++ src/main/java/ru/netology/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology;
+
+public class FormDate {
+    public int day = 13;
+    public int month = 6;
+    public int year = 1999;
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Post {
+    public String name = "Иван";
+    public String passport = "4444 №44444444";
+    public String patronymic = "Иванович";
+    public String phone = "+7 (999)-999-99-99";
+    public String surname = "Иванова";
+    public boolean subscription = true;
+    public FormDate birthday = new FormDate();
+
+
+}
